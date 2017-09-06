1ff32dbfab837b1852c2f77de9542a845a2240e4
Subuly01 <subhrashis.x.ganguly2@aexp.com>
2017-09-06 12:39:23 +0530

    test

---
+++ Artmaven/src/main/java/com/test/InitialClass.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class InitialClass {
+
+	public InitialClass() {
+		// TODO Auto-generated constructor stub
+		
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
